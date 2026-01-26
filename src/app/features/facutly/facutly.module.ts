3045d7a5aeefaad1b52e8c99535025d3de70a764
Varun-San <sathiyapriya1718@gmail.com>
2026-01-26 16:22:10 +0530

    Created folder sturcture

---
+++ src/app/features/facutly/facutly.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FacutlyRoutingModule } from './facutly-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FacutlyRoutingModule
+  ]
+})
+export class FacutlyModule { }
---
+++ src/app/features/notices/notices.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NoticesRoutingModule } from './notices-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NoticesRoutingModule
+  ]
+})
+export class NoticesModule { }
